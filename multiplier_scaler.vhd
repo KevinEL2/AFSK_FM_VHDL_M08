ea9a98b52ef9a727cf78619d3a41ac6998401ca2
KevinEL2 <113671597+KevinEL2@users.noreply.github.com>
2024-01-24 13:35:35 +0100

    Add files via upload

--- multiplier_scaler.vhd
+++ multiplier_scaler.vhd
@@ -21,10 +21,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,14 +33,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity multiplier_scaler is
-    Port ( SignalA : in STD_LOGIC;
-           SignalB : in STD_LOGIC;
-           Signal_out : out STD_LOGIC);
+    Port ( SignalA : in STD_LOGIC_vector(8 downto 0);
+           SignalB : in STD_LOGIC_vector (8 downto 0);
+           Signal_out : out STD_LOGIC_vector(8 downto 0)
+          );
 end multiplier_scaler;
 
 architecture Behavioral of multiplier_scaler is
-
+Signal temp : std_logic_vector (17 downto 0);
 begin
-
-
+process(signalA,SignalB,temp)
+begin 
+    temp <= SignalA * SignalB;
+    Signal_out <= temp(16 downto 8);
+end process;
 end Behavioral;
